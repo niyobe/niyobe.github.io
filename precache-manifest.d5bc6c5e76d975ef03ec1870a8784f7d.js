4e71027eb427ae5fc87a8305a895ff41c70f496d
Niyobe <niyobe021@gmail.com>
2019-07-18 02:24:06 +0900

    Updates

---
+++ precache-manifest.d5bc6c5e76d975ef03ec1870a8784f7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391dd0d56ba0ecca38ba00d4800cc254",
+    "url": "localhost/index.html"
+  },
+  {
+    "revision": "8a4651ca82b2d97f872a",
+    "url": "localhost/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ce45e9e9a30cffe5c4ec",
+    "url": "localhost/static/js/2.099e78af.chunk.js"
+  },
+  {
+    "revision": "8a4651ca82b2d97f872a",
+    "url": "localhost/static/js/main.7399415d.chunk.js"
+  },
+  {
+    "revision": "8d97849a518faca1f502",
+    "url": "localhost/static/js/runtime~main.b947f22c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "localhost/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a8e5dd735be4e5ef240ce849780a56b.js"
+  "localhost/precache-manifest.d5bc6c5e76d975ef03ec1870a8784f7d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("localhost/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
